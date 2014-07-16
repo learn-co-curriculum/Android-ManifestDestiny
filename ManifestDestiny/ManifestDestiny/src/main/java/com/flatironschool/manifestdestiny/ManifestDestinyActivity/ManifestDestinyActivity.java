b09eee1d778beccf3048c05136dafb6ba6ffbd76
altyus <alctyus@gmail.com>
2014-07-16 12:47:44 -0400

    added project solution

---
+++ ManifestDestiny/ManifestDestiny/src/main/java/com/flatironschool/manifestdestiny/ManifestDestinyActivity/ManifestDestinyActivity.java
@@ -0,0 +1,22 @@
+package com.flatironschool.manifestdestiny.ManifestDestinyActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by altyus on 7/15/14.
+ */
+public class ManifestDestinyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(android.R.layout.ac);
+        //setContentView(android.R.layout.activity_manifestdestiny);
+//        setContentView(android.R.layout.activity_manifestdestiny);
+       //setContentView(android.R.layout.activity_manifestdestiny);
+    }
+}
